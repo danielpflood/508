5bf9d6072598175680a1feb2566093beccf41145
Daniel Flood <danielpflood@gmail.com>
2012-12-03 22:02:11 -0500

    ok

--- new/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysql_hostname = "localhost";
-$mysql_user = "508";
-$mysql_password = "508devDBpass";
-$mysql_database = "508devDB";
-$bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Opps some thing went wrong with connection");
-mysql_select_db($mysql_database, $bd) or die("Opps some thing went wrong! can't select db!");
-
-?>
--- new/form.php
+++
@@ -1,4 +0,0 @@
-<form id="newproj" name="newpeoj" method="post" action="newproject.php">
-	Project Name <input name="project_name" type="text" id="project_name" value="" size="42" />
-<input name="createproject" type="submit" id="createproject" value="create project" />
-</form>
