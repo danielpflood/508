55745b9604e73f974a576f44c50ca57c04b7c7c2
Daniel Flood <danielpflood@gmail.com>
2014-02-18 19:23:37 +0000

    changed style to white background and fixed syntax of loggedin

--- loggedin.php
+++ loggedin.php
@@ -1,5 +1,7 @@
 <div id="loggedinbox">
-  	<a href="/508/users/<?php echo htmlspecialchars($_SESSION['username']) ?>"><?php echo htmlspecialchars($_SESSION['username']) ?>
+  	<?php 
+  	echo '<a href="/508/users/'.htmlspecialchars($_SESSION['username']).'">'; 
+  	echo htmlspecialchars($_SESSION['username']).'</a>' ?>
   	<a href="/508/messages/" > Messages</a>
   	<a href="/508/projects/"> Projects</a>
   	<a href="/508/users/"> Users</a>
